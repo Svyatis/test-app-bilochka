aa781703d2a878e40c5d0b6335946fae164bb76a
Sviatoslav <svyatis.lviv@gmail.com>
2023-07-29 15:08:10 +0300

    cleaning

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyABYfhB3dDhJlQElu9sYP0MUi3cwlZhMN8',
+    authDomain: 'bilochka-c73c4.firebaseapp.com',
+    databaseURL: 'https://bilochka-c73c4.firebaseio.com',
+    projectId: 'bilochka-c73c4',
+    storageBucket: 'bilochka-c73c4.appspot.com',
+    messagingSenderId: '1081266342112',
+    appId: '1:1081266342112:web:9fc4466a54c31018536885',
+    measurementId: 'G-LCFYM55GGD'
+  }
+};
